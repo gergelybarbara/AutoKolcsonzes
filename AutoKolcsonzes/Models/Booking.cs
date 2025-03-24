959eacf16f8d683afc269cea2dfb7d86168b9ccd
Futty Imre <ap@d.dd>
2025-03-24 19:29:21 +0100

    Add project files.

---
+++ AutoKolcsonzes/Models/Booking.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKolcsonzes.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int CarId { get; set; }
+        public int TotalPrice { get; set; }
+        public string UserUID  { get; set; }
+
+        public Booking(string sor)
+        {
+            var t= sor.Split(";");
+
+            Id = Convert.ToInt32(t[0]);
+            StartDate = Convert.ToDateTime(t[1]);
+            EndDate = Convert.ToDateTime(t[2]); 
+            CarId = Convert.ToInt32(t[3]);
+            TotalPrice = Convert.ToInt32(t[4]); ;
+            UserUID = t[5];
+        }
+
+        public override string? ToString()
+        {
+            return $"{CarId} ({StartDate:yyyy-MM-dd} - {EndDate:yyyy-MM-dd}";
+        }
+    }
+}
---
+++ AutoKolcsonzes/Models/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoKolcsonzes.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int Year { get; set; }
+        public int DailyPrice { get; set; }
+
+        public Car(string sor)
+        {
+            var t = sor.Split(";");
+            Id = Convert.ToInt32(t[0]);
+            Brand = t[1];
+            Model = t[2];
+            LicensePlate = t[3];
+            Year = Convert.ToInt32(t[4]);
+            DailyPrice = Convert.ToInt32(t[5]);
+        }
+
+        public override string? ToString()
+        {
+            return $"{LicensePlate}-{Brand}";
+        }
+    }
+}
